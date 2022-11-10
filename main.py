638bae133945f96b1e71431bab4c6a195b3076a4
n3kosempai <alvarom12098@gmail.com>
2022-11-10 02:59:30 -0500

    For test purposes only
    This is a tes to test if it renders the image in android with the absence of libavif

---
+++ main.py
@@ -0,0 +1,38 @@
+from kivy.lang import Builder
+import pillow_avif
+from kivymd.app import MDApp
+
+
+
+KV = '''
+MDScreen:
+
+    MDSmartTile:
+        radius: 24
+        box_radius: [0, 0, 24, 24]
+        box_color: 1, 1, 1, .2
+        source: "goldfinch.avif"
+        pos_hint: {"center_x": .5, "center_y": .5}
+        size_hint: None, None
+        size: "320dp", "320dp"
+
+        MDIconButton:
+            icon: "heart-outline"
+            theme_icon_color: "Custom"
+            icon_color: 1, 0, 0, 1
+            pos_hint: {"center_y": .5}
+            on_release: self.icon = "heart" if self.icon == "heart-outline" else "heart-outline"
+
+        MDLabel:
+            text: "very first test"
+            bold: True
+            color: 1, 1, 1, 1
+'''
+
+
+class MyApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+
+MyApp().run()
